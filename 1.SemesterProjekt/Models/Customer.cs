d0458d6412bb012a2c3cec50ea896f2c7641222c
anhtran22 <anhtran_91@hotmail.com>
2023-05-18 13:25:57 +0200

    SU1-13

--- 1.SemesterProjekt/Models/Customer.cs
+++ 1.SemesterProjekt/Models/Customer.cs
@@ -8,5 +8,21 @@ namespace _1.SemesterProjekt.Models
 {
     public class Customer
     {
+        public Customer(int id, string name, string address, string phoneNo, string email, bool isDeleted) 
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Address = address;
+            this.PhoneNo = phoneNo;
+            this.Email = email;
+            this.IsDeleted = isDeleted;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
