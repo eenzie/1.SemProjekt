e5974038459a707ea1f48ee8e82fc1d493051cfa
eenzie <100802909+eenzie@users.noreply.github.com>
2023-05-21 11:06:07 +0200

    SU1-47 Service layer method for Update (Edit) Customer

--- 1.SemesterProjekt/Service/CustomerService.cs
+++ 1.SemesterProjekt/Service/CustomerService.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using _1.SemesterProjekt.Models;
+using _1.SemesterProjekt.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1.SemesterProjekt.Service {
-    public class CustomerService {
+namespace _1.SemesterProjekt.Service 
+{
+    public class CustomerService 
+    {
+        public bool EditCustomerByID (string id, Customer updatedCustomer)
+        {
+            Database database = new Database();
+            bool isUpdated = database.UpdateCustomerByID(id, updatedCustomer);
+            return isUpdated;
+        }
+
     }
 }
