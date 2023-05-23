e68bd6ab8575900e5ad49624459694ad103c9ac8
anhtran22 <anhtran_91@hotmail.com>
2023-05-23 16:03:06 +0200

    SU1-32

--- 1.SemesterProjekt/Models/Eyetest.cs
+++ 1.SemesterProjekt/Models/Eyetest.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace _1.SemesterProjekt.Models
 {
-    internal class Eyetest
+    public class Eyetest
     {
+        /// <summary>
+        /// Written by Anh
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="price"></param>
+        /// <param name="customerID"></param>
+        public Eyetest(int id, decimal price, int customerID) 
+        {
+            this.ID = id;
+            this.Price = price;
+            this.CustomerID = customerID;
+        }
+        public int ID { get; set; }
+        public decimal Price { get; set; }
+        public int CustomerID { get; set; }
     }
 }
