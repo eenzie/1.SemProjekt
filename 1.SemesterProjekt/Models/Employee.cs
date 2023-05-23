e4a2bfbcfb7bb0848967f6b719fa25d1b2ca4c2b
anhtran22 <anhtran_91@hotmail.com>
2023-05-23 16:12:25 +0200

    SUI1-30

--- 1.SemesterProjekt/Models/Employee.cs
+++ 1.SemesterProjekt/Models/Employee.cs
@@ -8,5 +8,24 @@ namespace _1.SemesterProjekt.Models
 {
     public class Employee
     {
+        /// <summary>
+        /// Written by Anh
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="phone"></param>
+        /// <param name="shopID"></param>
+        public Employee(int id, string name, string phone, int shopID) 
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Phone = phone;
+            this.ShopID = shopID;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int ShopID { get; set; }
     }
 }
