146ba7c6226e3d737b60cfd671d17609ec264297
eenzie <100802909+eenzie@users.noreply.github.com>
2023-05-24 17:44:22 +0200

    SU1-28 Defined Order (constructor) and OrderLine classes

--- 1.SemesterProjekt/Models/Order.cs
+++ 1.SemesterProjekt/Models/Order.cs
@@ -8,6 +8,28 @@ namespace _1.SemesterProjekt.Models
 {
     public class Order
     {
+        /// <summary>
+        /// Written by Ina
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="date"></param>
+        /// <param name="subTotal"></param>
+        /// <param name="customer"></param>
+        /// <param name="employee"></param>
+        /// <param name="shop"></param>
+        public Order(int id, DateTime date, decimal subTotal, Customer customer, Employee employee, Shop shop)
+        {
+            this.ID = id;
+            this.Date = date;
+            this.SubTotal = subTotal;
+            this.Customer = customer;
+            this.Employee = employee;
+            this.Shop = shop;
+        }
+
+        /// <summary>
+        /// Written by Anton
+        /// </summary>
         public int ID { get; set; }
         public DateTime Date { get; set; }
         public decimal SubTotal { get; set; }
--- 1.SemesterProjekt/Models/OrderLine.cs
+++ 1.SemesterProjekt/Models/OrderLine.cs
@@ -8,5 +8,30 @@ namespace _1.SemesterProjekt.Models
 {
     public class OrderLine
     {
+        /// <summary>
+        /// Witten by Ina
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="quantity"></param>
+        /// <param name="salesPrice"></param>
+        /// <param name="productID"></param>
+        /// <param name="eyetestID"></param>
+        /// <param name="orderID"></param>
+        public OrderLine(int id, int quantity, decimal salesPrice, int productID, int eyetestID, int orderID)
+        {
+            this.ID = id;
+            this.Quantity = quantity;
+            this.SalesPrice = salesPrice;
+            this.ProductID = productID;
+            this.eyetestID = eyetestID;
+            this.orderID = orderID;
+        }
+
+        public int ID { get; set; }
+        public int Quantity { get; set; }
+        public decimal SalesPrice { get; set; }
+        public int ProductID { get; set; }
+        public int eyetestID { get; set; }
+        public int orderID { get; set; }
     }
 }
